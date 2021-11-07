4c4220cabfcd558b2b6f3e0cbf376f0375ad7f32
Jason Ebblewhite <jasonebblewhite@gmail.com>
2021-11-07 11:48:42 +0000

    restructure files and plan for modularisation

---
+++ Bookiebot/Databaser.py
@@ -0,0 +1,26 @@
+"""
+Database initialising class
+"""
+
+import json
+import pandas as pd
+from sqlalchemy import create_engine
+from operator import itemgetter
+
+class Databaser:
+    DATABASE_TYPE = 'postgresql'
+    DBAPI = 'psycopg2'
+    PORT = 5432
+    DATABASE = 'postgres'
+    def __init__(self):
+        d = self._load_file('db_credentials.json')
+        self.ENDPOINT, self.USER, self.PASSWORD = itemgetter('ENDPOINT', 'USER', 'PASSWORD')(d)
+        self.engine = create_engine(f"{self.DATABASE_TYPE}+{self.DBAPI}://{self.USER}:{self.PASSWORD}@{self.ENDPOINT}:{self.PORT}/{self.DATABASE}")
+        self.engine.connect()
+
+    def _load_file(self, infile: str) -> dict:
+        with open(infile,'r+') as file:
+            return json.load(file)
+
+    def push_to_db(self, df: pd.DataFrame, db_name: str):
+        df.to_sql(db_name, self.engine, if_exists='replace')
